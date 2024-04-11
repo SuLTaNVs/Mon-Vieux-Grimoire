61796c90803c219bafc6b333d31c206617a2870c
Mehdi BELAZZOUG <belazzoug.mehdi@hotmail.com>
2024-04-11 18:52:55 +0200

    All controlers updated

--- Backend/router/globalRoute.js
+++ Backend/router/globalRoute.js
@@ -5,10 +5,24 @@ const auth = require("../middlewares/auth");
 
 // Définition des routes
 router.get("/", globalController.getAllBooks); // Route pour obtenir tous les livres
-router.get("/:id", globalController.getOneBook); // Route pour obtenir un livre spécifique
 router.get("/bestrating", globalController.getBestBooks); // Route pour obtenir les 3 meilleurs livres
-router.post("/", auth, globalController.createBook); // Route pour créer un livre
-router.put("/:id", auth, globalController.modifyBook); // Route pour modifier un livre
+router.get("/:id", globalController.getOneBook); // Route pour obtenir un livre spécifique
+// Route pour créer un livre
+router.post(
+  "/",
+  auth,
+  multer.upload,
+  multer.optimizeImage,
+  globalController.createBook
+);
+// Route pour modifier un livre
+router.put(
+  "/:id",
+  auth,
+  multer.upload,
+  multer.optimizeImage,
+  globalController.modifyBook
+);
 router.delete("/:id", auth, globalController.deleteBook); // Route pour supprimer un livre
 router.post("/:id/rating", auth, globalController.rateBook); // Route pour noter un livre
 
